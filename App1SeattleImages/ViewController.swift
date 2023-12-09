4e7769ad37f38a73c951b8454c4b6339ea3db8fb
Yarong Wang <wangyarong1266@gmail.com>
2023-12-09 15:34:55 -0800

    Initial Commit

---
+++ App1SeattleImages/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  App1SeattleImages
+//
+//  Created by rongbaobao888 on 12/9/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
